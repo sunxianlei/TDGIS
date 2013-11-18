2bac17e0058d47aa460c338dd0e5b39a905ef4aa
sunxianlei <sunxianlei@foxmail.com>
2013-11-18 20:59:48 +0800

    add offline data with some bugs

--- src/cn/sunxianlei/tdgis/activity/LocateExampleActivity.java
+++ src/cn/sunxianlei/tdgis/activity/LocateExampleActivity.java
@@ -1,16 +1,25 @@
 package cn.sunxianlei.tdgis.activity;
 
+import com.esri.android.map.MapView;
+import com.esri.android.map.ags.ArcGISLocalTiledLayer;
+
 import cn.sunxianlei.tdgis.R;
 import android.app.Activity;
 import android.os.Bundle;
 
 public class LocateExampleActivity extends Activity {
-
+	MapView mapView=null;
+	ArcGISLocalTiledLayer localTiledLayer;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_locate);
+		
+		mapView=(MapView)findViewById(R.id.mapviewInLocateActivity);
+		localTiledLayer=new ArcGISLocalTiledLayer("file://mnt/sdcard/layers");
+		mapView.addLayer(localTiledLayer);
+		
 	}
 
 }
