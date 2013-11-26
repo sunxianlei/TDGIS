df254dd5683972623425ccd31092ca4d7a76ca47
sunxianlei <sunxianlei@foxmail.com>
2013-11-26 22:21:44 +0800

    change activity vector ui

--- src/cn/sunxianlei/tdgis/activity/VectorExampleActivity.java
+++ src/cn/sunxianlei/tdgis/activity/VectorExampleActivity.java
@@ -1,43 +1,44 @@
 package cn.sunxianlei.tdgis.activity;
 
 import com.esri.android.map.MapView;
+import com.esri.android.map.ags.ArcGISTiledMapServiceLayer;
 import com.esri.android.map.event.OnLongPressListener;
 import com.esri.core.geometry.Point;
 
 import cn.sunxianlei.tdgis.R;
 import cn.sunxianlei.tdgis.layer.GoogleMapsOnlineLayer;
+import cn.sunxianlei.tdgis.layer.MBTilesGoogleMapsOfflineLayer;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TabHost;
 import android.widget.Toast;
 
 public class VectorExampleActivity extends Activity {
 
 	private MapView mapView=null;
+	TabHost mTabHost;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_vector);
-		mapView=(MapView)findViewById(R.id.mapviewInVectorActivity);
 		
-		GoogleMapsOnlineLayer googleMapsLayer=new GoogleMapsOnlineLayer(GoogleMapsOnlineLayer.MapType.VECTOR, null, true);
-		mapView.addLayer(googleMapsLayer);
-		
-		mapView.setOnLongPressListener(new OnLongPressListener() {
-			
-			@Override
-			public boolean onLongPress(float x, float y) {
-				// TODO Auto-generated method stub
-				if (mapView.isLoaded()) {
-					Point pt=mapView.toMapPoint(x,y);
-					String text="X:"+pt.getX()+"Y:"+pt.getY();
-					Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
+		mTabHost = (TabHost) findViewById(R.id.tabHost);
+		mTabHost.setup();
 
-					return true;
-				}
-				return false;
-			}
-		});
+		TabHost.TabSpec editSpec = mTabHost.newTabSpec("Edit");
+		editSpec.setContent(R.id.offlinereditlayout);
+		editSpec.setIndicator("±à¼­");
+		mTabHost.addTab(editSpec);
+
+		TabHost.TabSpec syncSpec = mTabHost.newTabSpec("Sync");
+		syncSpec.setContent(R.id.offlinersynclayout);
+		syncSpec.setIndicator("Í¬²½");
+		mTabHost.addTab(syncSpec);
+		
+		mapView = ((MapView) findViewById(R.id.map));
+		MBTilesGoogleMapsOfflineLayer mbTilesGoogleMapsOfflineLayer=new MBTilesGoogleMapsOfflineLayer(getApplicationContext());
+		mapView.addLayer(mbTilesGoogleMapsOfflineLayer);
 	}
 
 }
